204431c4cacdba2cd472152b8e345274942f5f55
ksu228 <108416143+ksu228@users.noreply.github.com>
2022-07-31 20:31:48 +0300

    Add files via upload

---
+++ simpleapp/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import Product
+class ProductForm(forms.ModelForm):
+   class Meta:
+       model = Product
+       fields = [
+           'name',
+           'description',
+           'quantity',
+           'category',
+           'price',
+       ]
+
+   def clean(self):
+       cleaned_data = super().clean()
+       description = cleaned_data.get("description")
+       if description is not None and len(description) < 20:
+           raise ValidationError({
+               "description": "Описание не может быть менее 20 символов."
+           })
+
+       name = cleaned_data.get("name")
+       if name == description:
+           raise ValidationError(
+               "Описание не должно быть идентично названию."
+           )
+
+       return cleaned_data
